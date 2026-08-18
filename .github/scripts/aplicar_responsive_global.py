cb3cc26ba566c48a18645b90233d9226c51d4c08
danielmm007-commits <danielmm007@gmail.com>
2026-08-18 08:20:43 -0500

    Integrar documento en confirmacion de domicilio

--- .github/scripts/aplicar_responsive_global.py
+++ .github/scripts/aplicar_responsive_global.py
@@ -5,6 +5,7 @@ link='<link rel="stylesheet" href="responsive-global.css?v=20260818-2305">'
 gps='<script src="gps-aceptacion-v1.js?v=20260818-2338"></script>'
 hitos='<script src="gps-hitos-tecnico-v1.js?v=20260818-0635"></script>'
 flex='<script src="domicilio-flujo-flex-v1.js?v=20260818-0816"></script>'
+docdom='<script src="domicilio-documento-v1.js?v=20260818-0818"></script>'
 plan='<script src="plan-catalogo-v1.js?v=20260818-0721"></script>'
 cedula='<script src="cedula-miniaturas-v1.js?v=20260818-0758"></script>'
 ident='<script src="ejecucion-identificacion-v1.js?v=20260818-0803"></script>'
@@ -22,11 +23,12 @@ for p in Path('.').glob('*.html'):
         if '</body>' in s:
             s=s.replace('</body>',gps+hitos+'</body>',1)
     if p.name=='instalacion-domicilio.html':
-        s=re.sub(r'<script src="domicilio-flujo-flex-v1\.js\?v=[^"]+"></script>','',s)
-        if '</body>' in s:s=s.replace('</body>',flex+'</body>',1)
+        s=re.sub(r'<script src="domicilio-flujo-flex-v1\.js\?v=[^"]+\"></script>','',s)
+        s=re.sub(r'<script src="domicilio-documento-v1\.js\?v=[^"]+\"></script>','',s)
+        if '</body>' in s:s=s.replace('</body>',flex+docdom+'</body>',1)
     if p.name=='instalacion-ejecucion.html':
-        s=re.sub(r'<script src="plan-catalogo-v1\.js\?v=[^"]+"></script>','',s)
-        s=re.sub(r'<script src="cedula-miniaturas-v1\.js\?v=[^"]+"></script>','',s)
-        s=re.sub(r'<script src="ejecucion-identificacion-v1\.js\?v=[^"]+"></script>','',s)
+        s=re.sub(r'<script src="plan-catalogo-v1\.js\?v=[^"]+\"></script>','',s)
+        s=re.sub(r'<script src="cedula-miniaturas-v1\.js\?v=[^"]+\"></script>','',s)
+        s=re.sub(r'<script src="ejecucion-identificacion-v1\.js\?v=[^"]+\"></script>','',s)
         if '</body>' in s:s=s.replace('</body>',plan+cedula+ident+'</body>',1)
     p.write_text(s,encoding='utf-8')
