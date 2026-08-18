c2f1ae16fcaa49c5d1e2ec2e66f274662f4e6266
danielmm007-commits <danielmm007@gmail.com>
2026-08-18 10:00:00 -0500

    Actualizar cache de trabajos activos sin duplicados

--- .github/scripts/aplicar_responsive_global.py
+++ .github/scripts/aplicar_responsive_global.py
@@ -4,7 +4,7 @@ import re
 link='<link rel="stylesheet" href="responsive-global.css?v=20260818-2305">'
 gps='<script src="gps-aceptacion-v1.js?v=20260818-2338"></script>'
 hitos='<script src="gps-hitos-tecnico-v1.js?v=20260818-0635"></script>'
-activos='<script src="trabajos-vista-activos-v1.js?v=20260818-0931"></script>'
+activos='<script src="trabajos-vista-activos-v1.js?v=20260818-0959"></script>'
 flex='<script src="domicilio-flujo-flex-v1.js?v=20260818-0904"></script>'
 docdom='<script src="domicilio-documento-v1.js?v=20260818-0818"></script>'
 contacto='<script src="domicilio-contacto-final-v1.js?v=20260818-0824"></script>'
