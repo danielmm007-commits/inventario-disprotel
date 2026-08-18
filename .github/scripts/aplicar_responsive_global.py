38e6ae2a083216c568e098d37199723447ae1f1e
danielmm007-commits <danielmm007@gmail.com>
2026-08-18 08:18:50 -0500

    Actualizar cache validacion en vivo

--- .github/scripts/aplicar_responsive_global.py
+++ .github/scripts/aplicar_responsive_global.py
@@ -4,7 +4,7 @@ import re
 link='<link rel="stylesheet" href="responsive-global.css?v=20260818-2305">'
 gps='<script src="gps-aceptacion-v1.js?v=20260818-2338"></script>'
 hitos='<script src="gps-hitos-tecnico-v1.js?v=20260818-0635"></script>'
-flex='<script src="domicilio-flujo-flex-v1.js?v=20260818-0812"></script>'
+flex='<script src="domicilio-flujo-flex-v1.js?v=20260818-0816"></script>'
 plan='<script src="plan-catalogo-v1.js?v=20260818-0721"></script>'
 cedula='<script src="cedula-miniaturas-v1.js?v=20260818-0758"></script>'
 ident='<script src="ejecucion-identificacion-v1.js?v=20260818-0803"></script>'
